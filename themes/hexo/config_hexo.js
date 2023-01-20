bf8dc1663fdc95fd12ae0bd2e96e11a93a5daf36
zhongs <572662612@qq.com>
2023-01-20 15:49:38 +0800

    修改图标，hexo&mastery主题首页动态内容

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['Spell at all costs， to rush you in the future.', 'Hi，这里是OscarZs的博客。', '小小萌新，请多指教orz.', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
 
