57c825b8a6cf3a37706353c3d5d005075b658d07
zhongs <572662612@qq.com>
2023-01-24 15:28:11 +0800

    blog style

--- blog.config.js
+++ blog.config.js
@@ -76,7 +76,7 @@ const BLOG = {
   // 悬浮挂件
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
   WIDGET_PET_LINK: 'https://imuncle.github.io/live2d/model/22/model.2017.cba-normal.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
-  WIDGET_PET_SWITCH_THEME: true, // 点击宠物挂件切换博客主题
+  WIDGET_PET_SWITCH_THEME: false, // 点击宠物挂件切换博客主题
 
   // 音乐播放插件
   MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
